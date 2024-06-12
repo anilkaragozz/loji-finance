aa2afd8f1303162ff30b53d5f2287f422320177b
anilkaragoz <anil.karagoz@d-teknoloji.com.tr>
2024-06-12 04:19:40 +0300

    chore(page layout): add page layout

--- src/PrivateRouter.tsx
+++ src/PrivateRouter.tsx
@@ -1,11 +1,23 @@
 import { Outlet, Navigate } from "react-router-dom";
 import { useAuth } from "@/context/AuthContext";
+import Navbar from "@/components/general/navbar";
 
 const PrivateRoute = () => {
   const { state } = useAuth();
   const { accessToken } = state;
 
-  return accessToken ? <Outlet /> : <Navigate to="/login" />;
+  return accessToken ? (
+    <>
+      <Navbar />
+      <div className="flex min-h-screen w-full flex-col">
+        <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+          <Outlet />
+        </main>
+      </div>
+    </>
+  ) : (
+    <Navigate to="/login" />
+  );
 };
 
 export default PrivateRoute;
